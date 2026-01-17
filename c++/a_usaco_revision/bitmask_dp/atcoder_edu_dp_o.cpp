02c8adcbbf03fe5268b0d4c3b3612f29db9d4401
ltn18 <nguyentamhoanglam@gmail.com>
2026-01-16 20:08:56 -0800

    atcoder edu dp o (interesting)
    Signed-off-by: ltn18 <nguyentamhoanglam@gmail.com>

--- c++/a_usaco_revision/bitmask_dp/atcoder_edu_dp_o.cpp
+++ c++/a_usaco_revision/bitmask_dp/atcoder_edu_dp_o.cpp
@@ -20,7 +20,33 @@ const int mod = 1e9+7;
 
 void solve() {
     int n; cin >> n;
-    
+
+    vi compat(n, 0);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int a; cin >> a;
+            if (a) compat[i] |= (1 << j);
+        }
+    }
+
+    int FULL = 1 << n;
+
+    /*
+        dp[S] = #ways to match females in subset S with the first |S| men
+        dp[S] += dp[S\{w}] if last man is compatible with w
+    */
+    vi dp(FULL, 0);
+    dp[0] = 1;
+
+    for (int mask = 0; mask < FULL; mask++) {
+        int lastman = __builtin_popcount(mask);
+        for (int w = 0; w < n; w++) {
+            if ((mask & (1 << w)) == 0) continue;
+            if ((compat[lastman-1] & (1 << w)) == 0) continue;
+            dp[mask] = (dp[mask] % mod + dp[mask ^ (1 << w)] % mod) % mod;
+        }
+    }
+    cout << dp[FULL-1] << '\n';
 }
 
 int main() {
