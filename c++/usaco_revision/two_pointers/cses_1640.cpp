c9d5ae0a87fbda2bed2066dc5ee042565dfc4bc8
ltn18 <nguyentamhoanglam@gmail.com>
2026-03-24 21:00:38 -0700

    two pointers
    Signed-off-by: ltn18 <nguyentamhoanglam@gmail.com>

---
+++ c++/usaco_revision/two_pointers/cses_1640.cpp
@@ -0,0 +1,50 @@
+// problem url: https://cses.fi/problemset/result/16674785/
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vii = vector< vector<int> >;
+
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FORD(i, a, b) for (int i = (a); i >= (b); i--)
+#define RFOR(i, n) for (int i = (n) - 1; i >= 0; i--)
+
+#define fi first
+#define se second
+#define pb push_back
+
+const int INF = 1e9+7;
+const int MOD = 1e9+7;
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+    
+    vi a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    map<int,int> pos;
+    for (int i = 0; i < n; i++) {
+        int target = x - a[i];
+        if (pos.count(target)) {
+            cout << (pos[target]+1) << ' ' << (i+1) << '\n';
+            return;
+        }
+        pos[a[i]] = i;
+    }
+
+    cout << "IMPOSSIBLE" << '\n';
+}
+
+int main() {
+    // freopen("name.in", "r", stdin);
+    // freopen("name.out", "w", stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tt = 1;
+    // cin >> tt;
+    while (tt--) solve();
+    return 0;
+}
